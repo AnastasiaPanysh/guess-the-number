aa194821322fa304472bba5161dd8d4325f2c880
anastasiya <nastya.panysh@gmail.com>
2022-09-16 22:18:13 +0300

    add js

---
+++ 1/app.js
@@ -0,0 +1,22 @@
+function searchBinary(value) {
+     let start = 0;
+     let end = 100;
+     let middle = Math.floor((start + end) / 2);
+     for (let i = 0; i < 100; i++) {
+
+
+
+//логика
+
+
+
+
+
+
+
+
+
+     }
+}
+
+console.log(searchBinary([(Math.floor(Math.random() * 100) + 1)]));
