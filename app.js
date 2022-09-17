ca1fc84e3fa7bd11c63935880719f64f4b58056a
anastasiya <nastya.panysh@gmail.com>
2022-09-17 22:38:05 +0300

    add js

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 function doRandom() {
-     const randomNum = (Math.floor(Math.random() * 100) + 1)
-     const hello = prompt('hi! Are you ready to play the game? Answer yes or no');
-     if (hello === 'yes') {
-          checkGuess(randomNum)
-     } else if (hello === 'no') {
-          alert('but maybe yes?');
-     } else {
-          alert('Incorrect input, please try again. Answer yes or no');
-     }
-
-}
-
-function checkGuess(num) {
-     let stepCounter = 1;
-     let game = +prompt('Guess a number from 1 to 100');
+     // const randomNum = (Math.floor(Math.random() * 100) + 1)
+     let hello = prompt('hi! Are you ready to play the game? Answer yes or no');
      for (;;) {
-          if (game === num) {
-               break;
-          } else if (game <= num) {
-               // alert('too little, try again');
-               stepCounter++
-               game = +prompt('Too little, try again');
+          if (hello === 'yes') {
+               checkGuess()
+          } else if (hello === 'no') {
+               hello = prompt('but maybe yes?');
                continue;
-          } else if (game >= num) {
-               // alert('too big, try again');
-               stepCounter++
-               game = +prompt('Too big, try again');
+          } else {
+               hello = prompt('Incorrect input, please try again. Answer yes or no');
                continue;
           }
      }
-     return alert(`Congratulations, you got it on the ${stepCounter}th try`);
+}
+
+function checkGuess() {
+     const randomNum = (Math.floor(Math.random() * 100) + 1)
+     let stepCounter = 1;
+     let game = +prompt('Guess a number from 1 to 100');
+     for (;;) {
+          if (!isNaN(game) && game > 0 && game < 100) {
+               if (game === randomNum) {
+                    break;
+               } else if (game <= randomNum) {
+                    stepCounter++
+                    game = +prompt('Too little, try again');
+                    continue;
+               } else if (game >= randomNum) {
+                    stepCounter++
+                    game = +prompt('Too big, try again');
+                    continue;
+               }
+          } else {
+               game = prompt('Incorrect input, please try again. Enter a number');
+          }
+     }
+     return game = +prompt(`Congratulations, you got it on the ${stepCounter}th try. Do you want to play again?`)
 }
 
 console.log(doRandom());
