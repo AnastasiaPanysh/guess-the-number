ec20478649872f5cda27153f88f1dcf58637f981
anastasiya <nastya.panysh@gmail.com>
2022-09-17 22:43:57 +0300

    add js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 function doRandom() {
      // const randomNum = (Math.floor(Math.random() * 100) + 1)
-     let hello = prompt('hi! Are you ready to play the game? Answer yes or no');
+     let hello = prompt('Are you ready to play the game? Answer yes or no');
      for (;;) {
           if (hello === 'yes') {
                checkGuess()
           } else if (hello === 'no') {
                hello = prompt('but maybe yes?');
-               continue;
           } else {
                hello = prompt('Incorrect input, please try again. Answer yes or no');
-               continue;
           }
      }
 }
@@ -25,17 +23,16 @@ function checkGuess() {
                } else if (game <= randomNum) {
                     stepCounter++
                     game = +prompt('Too little, try again');
-                    continue;
                } else if (game >= randomNum) {
                     stepCounter++
                     game = +prompt('Too big, try again');
-                    continue;
                }
           } else {
                game = prompt('Incorrect input, please try again. Enter a number');
           }
      }
      return game = +prompt(`Congratulations, you got it on the ${stepCounter}th try. Do you want to play again?`)
+
 }
 
 console.log(doRandom());
