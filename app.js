2e7a1ee1883bb8424643e8ecedf528e4a087d4df
anastasiya <nastya.panysh@gmail.com>
2022-09-17 21:39:34 +0300

    add js

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-const randomNum = (Math.floor(Math.random() * 100) + 1)
+function doRandom() {
+     const randomNum = (Math.floor(Math.random() * 100) + 1)
+     const hello = prompt('hi! Are you ready to play the game? Answer yes or no');
+     if (hello === 'yes') {
+          checkGuess(randomNum)
+     } else if (hello === 'no') {
+          alert('but maybe yes?');
+     } else {
+          alert('Incorrect input, please try again. Answer yes or no');
+     }
 
-const hello = prompt('hi! Are you ready to play the game? Answer yes or no');
-if (hello === 'yes') {
-     checkGuess(randomNum)
-} else {
-     alert('you lost');
-     // break;
 }
 
 function checkGuess(num) {
-     const stepCounter = 1;
-     const game = +prompt('Guess a number from 1 to 100');
-     if (game === num) {
-          alert(`Congratulations, you got it on the ${stepCounter} try`);
-     } else if (game < num) {
-          alert('too little, try again');
-          stepCounter++
-          return checkGuess(num);
-     } else if (game > num) {
-          alert('too big, try again');
-          stepCounter++
-          return checkGuess(num);
+     let stepCounter = 1;
+     let game = +prompt('Guess a number from 1 to 100');
+     for (;;) {
+          if (game === num) {
+               break;
+          } else if (game <= num) {
+               // alert('too little, try again');
+               stepCounter++
+               game = +prompt('Too little, try again');
+               continue;
+          } else if (game >= num) {
+               // alert('too big, try again');
+               stepCounter++
+               game = +prompt('Too big, try again');
+               continue;
+          }
      }
+     return alert(`Congratulations, you got it on the ${stepCounter}th try`);
 }
 
-console.log(checkGuess(randomNum));
+console.log(doRandom());
