9ce7a2f60d827755fba30f7e31a6e864ef7efe78
anastasiya <nastya.panysh@gmail.com>
2022-09-17 19:05:17 +0300

    add js

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-function searchBinary(value) {
-     let start = 0;
-     let end = 100;
-     let middle = Math.floor((start + end) / 2);
-     for (let i = 0; i < 100; i++) {
+const randomNum = (Math.floor(Math.random() * 100) + 1)
 
+const hello = prompt('hi! Are you ready to play the game? Answer yes or no');
+if (hello === 'yes') {
+     checkGuess(randomNum)
+} else {
+     alert('you lost');
+     // break;
+}
 
-
-//логика
-
-
-
-
-
-
-
-
-
+function checkGuess(num) {
+     const stepCounter = 1;
+     const game = +prompt('Guess a number from 1 to 100');
+     if (game === num) {
+          alert(`Congratulations, you got it on the ${stepCounter} try`);
+     } else if (game < num) {
+          alert('too little, try again');
+          stepCounter++
+          return checkGuess(num);
+     } else if (game > num) {
+          alert('too big, try again');
+          stepCounter++
+          return checkGuess(num);
      }
 }
 
-console.log(searchBinary([(Math.floor(Math.random() * 100) + 1)]));
+console.log(checkGuess(randomNum));
